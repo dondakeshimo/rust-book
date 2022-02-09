c00921c2cb61db058d545c9dfe019fc46bfbd6b9
dondakeshimo <went.went.takkun135@gmail.com>
2022-02-09 23:22:47 +0900

    feat: add variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("The value of x is: {}", x);
+    let x = 6;
+    println!("The value of x is: {}", x);
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    another_function();
+    println!("five: {}", five());
+
+    if five() > 5 {
+        println!("{} > 5", five());
+    } else if five() < 5 {
+        println!("{} < 5", five());
+    } else {
+        println!("{} = 5", five());
+    }
+
+    for num in (1..4).rev() {
+        println!("{}!", num)
+    }
+
+    for num in 1..4 {
+        println!("{}!", num)
+    }
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn five () -> i32 {
+    5
 }
