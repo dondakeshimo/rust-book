606a21ffb4f5bb6c40cf769ace9a2d6dc5e39848
dondakeshimo <went.went.takkun135@gmail.com>
2022-02-12 23:49:20 +0900

    feat: add integration_test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,42 +1,17 @@
+pub fn add_two(i: i32) -> i32 {
+    internal_adder(i, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn smaller_can_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-        assert!(larger.can_hold(&smaller));
-    }
-}
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2))
     }
 }
