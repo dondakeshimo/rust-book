b155349f8a22e9ab279d70591754b66fc007423b
dondakeshimo <went.went.takkun135@gmail.com>
2022-02-13 17:11:29 +0900

    feat: add searching

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,7 +8,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
@@ -31,3 +33,33 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents),
+        );
+    }
+}
